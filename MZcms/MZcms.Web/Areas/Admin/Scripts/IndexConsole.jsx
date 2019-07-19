9b1b74fab828817f2af7a69ca67a30fbbbcdd914
MiZone <mizonerom@hotmail.com>
2019-07-19 17:50:03 +0800

    add react framework

---
+++ MZcms/MZcms.Web/Areas/Admin/Scripts/IndexConsole.jsx
@@ -0,0 +1,10 @@
+﻿
+
+ReactDOM.render(
+    <div>
+        <antd.Button type="primary">Primary</antd.Button>
+        <antd.Button>Default</antd.Button>
+        <antd.Button type="dashed">Dashed</antd.Button>
+        <antd.Button type="danger">Danger</antd.Button>
+    </div>
+    , document.getElementById('root'));
